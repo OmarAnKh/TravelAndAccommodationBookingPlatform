eb4e5576ab40bd744f924854606eb25f0d674f5a
Omar Khalili <OmarKhalili810@gmail.com>
2025-06-23 22:32:39 +0300

    remove unused operations

--- TravelAndAccommodationBookingPlatform.Domain/Interfaces/IRefreshTokenRepository.cs
+++ TravelAndAccommodationBookingPlatform.Domain/Interfaces/IRefreshTokenRepository.cs
@@ -7,7 +7,6 @@ public interface IRefreshTokenRepository
     Task<RefreshToken?> GetByTokenAsync(string token);
 
     Task AddAsync(RefreshToken token);
-    Task UpdateAsync(RefreshToken token);
     Task RevokeTokenAsync(string token);
     Task RevokeAllTokensAsync(int userId);
 
--- TravelAndAccommodationBookingPlatform.Infrastructure/Repositories/RefreshTokenRepository.cs
+++ TravelAndAccommodationBookingPlatform.Infrastructure/Repositories/RefreshTokenRepository.cs
@@ -18,31 +18,14 @@ public class RefreshTokenRepository : IRefreshTokenRepository
     {
         return await _context.RefreshTokens.FirstOrDefaultAsync(t => t.Token == token);
     }
-
-    public async Task<RefreshToken?> GetByIdAsync(int id)
-    {
-        return await _context.RefreshTokens.FindAsync(id);
-    }
-
-    public async Task<IEnumerable<RefreshToken>> GetByUserIdAsync(int userId)
-    {
-        return await _context.RefreshTokens
-            .Where(t => t.UserId == userId)
-            .ToListAsync();
-    }
+    
 
     public async Task AddAsync(RefreshToken token)
     {
         await _context.RefreshTokens.AddAsync(token);
         await _context.SaveChangesAsync();
     }
-
-    public async Task UpdateAsync(RefreshToken token)
-    {
-        _context.RefreshTokens.Update(token);
-        await _context.SaveChangesAsync();
-    }
-
+    
     public async Task RevokeTokenAsync(string token)
     {
         var refreshToken = await GetByTokenAsync(token);
